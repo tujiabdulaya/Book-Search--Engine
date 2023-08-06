6df191098357c566bd5d9f88af7c24f07ce1c2f0
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-08-06 18:55:14 -0500

    added seachbook.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -11,6 +11,8 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK} from '../utils/mutation'
+import { useMutation } from '@apollo/client';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -59,6 +61,7 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook,{error}] = useMutation(SAVE_BOOK)
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
