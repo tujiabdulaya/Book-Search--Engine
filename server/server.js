7fa159f44a363f79149f2f60ae0b8b7306d4ab52
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-08-06 18:00:03 -0500

    added type defs

--- server/server.js
+++ server/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const {ApolloServer } = require('apollo-server-express')
+const {ApolloServer } = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
 
+const startServer = async() => {
+  const server = new ApolloServer({
+    context: authMiddleware
+
+  }) 
+  await server.start()
+} 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
