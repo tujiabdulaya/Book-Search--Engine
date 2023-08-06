5a2ed5dd446296c0865678b5bba8cc218e93abab
Tuji Abdulaya <tujiabdulaya1@gmail.com>
2023-08-06 18:16:39 -0500

    added index.js in schema

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -12,6 +12,59 @@ const resolvers = {
                 .select('__v -password')
                 return data;
             }
-        }
-    }
+        },
+        users: async () => {
+            return User.find().select("-__v -password").populate("savedBooks");
+          },
+          user: async (parent, { username }) => {
+            return User.findOne({ username })
+              .select("-__v -password")
+              .populate("savedBooks");
+          },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError("Incorrect credentials");
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError("Incorrect credentials");
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+        savedBooks: async (parent, { book }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $addToSet: { savedBooks: book } },
+              { new: true }
+            );
+            return updatedUser;
+          }
+          throw new AuthenticationError("You need to be logged in!");
+        },
+        removeBook: async (parent, { bookId }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId: bookId } } },
+              { new: true }
+            );
+            return updatedUser;
+          }
+        },
+      },
 }
